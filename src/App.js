27c1adf711e2f45dcd729ec1c9c15c2431d89f55
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-09-19 21:06:52 +0600

    second day in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import './MyStyle.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <UserDataLoad></UserDataLoad>
     </div>
   );
+}
+
+
+// api data load 
+
+function UserDataLoad() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+
+  return (
+    <div className='gride-style' >
+      {
+        users.map(user => <DisplayUser name={user.name} email={user.email}></DisplayUser>)
+      }
+    </div>
+  )
+};
+
+
+
+// data display in UI
+
+function DisplayUser(props) {
+  const { name, email } = props;
+
+  const [count, setCount] = useState(0);
+
+
+  const moreDetals = () => {
+    const newValu = count + 1;
+    setCount(newValu);
+  }
+
+  return (
+
+    <div className="BoxStyle">
+      <h3>Name : {name}</h3>
+      <h5>Email : {email}</h5>
+      <p> Count Valu : {count}</p>
+      <button className="btn-style" onClick={moreDetals}> More.. </button>
+    </div>
+
+  )
 }
 
 export default App;
